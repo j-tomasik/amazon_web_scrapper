e8181a2cfb92b3ec6616116eb49b3ec2281b5bc0
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-12-20 14:34:17 -0800

    env file for api key

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const request = require('request-promise')
+require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const apiKey = process.env.APIKEY
+
+console.log(apiKey)
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
