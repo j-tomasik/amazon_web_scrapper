e40a861ed14e6ced1f29f4129490405f31a6e0c9
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-12-20 14:56:35 -0800

    route for product offers

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Amazon Scrapper API')
 })
 
-//GET product details
+//GET product Details
 app.get('/products/:productId', async (req, res) => {
     const { productId } = req.params;
 
@@ -29,12 +29,25 @@ app.get('/products/:productId', async (req, res) => {
     }
 })
 
-//GET product reviews
+//GET product Reviews
 app.get('/products/:productId/reviews', async (req, res) => {
     const { productId } = req.params;
 
     try {
         const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`)
+    
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+//GET product Offers
+app.get('/products/:productId/offers', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`)
     
         res.json(JSON.parse(response))
     } catch (error) {
