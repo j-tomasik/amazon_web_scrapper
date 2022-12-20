d460adb4fc1e8b8b72a3b780290210cffc268f27
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-12-20 14:43:33 -0800

    suceesfully has product details route

--- index.js
+++ index.js
@@ -7,13 +7,26 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const apiKey = process.env.APIKEY
 
-console.log(apiKey)
+
 const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Amazon Scrapper API')
+})
+
+//GET product details
+app.get('/products/:productId', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`)
+    
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+    }
 })
 
 app.listen(PORT, () => console.log(`Sever running on port ${PORT}`));
